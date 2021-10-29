dd374e3e077a7b6d20e215f6bc6209a3051a6a75
krishnajhapate <ki54.jhapate@gmail.com>
2021-10-29 15:08:05 +0530

    project setup

---
+++ src/pages/LandingPage/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+function LandingPage() {
+    return (
+        <div>
+            <nav class="notLoginNavbar navbar navbar-default navbar-static-top ">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button
+                            type="button"
+                            class="navbar-toggle collapsed"
+                            data-toggle="collapse"
+                            data-target="#navbar"
+                            aria-expanded="false"
+                            aria-controls="navbar"
+                        >
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="/">
+                            <img
+                                src="https://snakerspanel.com/images/vVdoIXNHrbgYT46UgJtKFF77a2xy9LNgLOE6kbPk.png"
+                                alt=""
+                                title=""
+                                // style="max-height: 80px;max-width:68px;"
+                            />
+                        </a>
+                    </div>
+                    <div id="navbar" class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active">
+                                <a href="/">Sign in</a>
+                            </li>
+                            <li>
+                                <a href="/services">Services</a>
+                            </li>
+                            <li>
+                                <a href="/page/terms-and-conditions">Terms</a>
+                            </li>
+                            <li>
+                                <a href="/register" class="singup">
+                                    Sign up
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default LandingPage;
