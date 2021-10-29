54b1ec43ae6ca605d3fd989cc6dfbd76aa6b5b2b
krishnajhapate <ki54.jhapate@gmail.com>
2021-10-29 15:22:56 +0530

    home page setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
     return (
         <div>
             <LandingPage />
+
+            <Router >
+              <Switch>
+                <Route path="/signup" />
+              </Switch>
+            </Router>
         </div>
     );
 }
