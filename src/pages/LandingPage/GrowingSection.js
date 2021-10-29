89e4e12627158a1494c768f3ab5ccb70b777e964
krishnajhapate <ki54.jhapate@gmail.com>
2021-10-29 15:43:02 +0530

    growinng section added

---
+++ src/pages/LandingPage/GrowingSection.js
@@ -0,0 +1,172 @@
+import React from "react";
+
+function GrowingSection() {
+    return (
+        <div>
+            <section class="growing-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h2>
+                                SnakersPanel smm panel <br /> Snakerspanel World
+                                Cheapest smm panel| smm reseller panel
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="growBox">
+                                <div class="growIcon">
+                                    <i class="fa fa-rocket"></i>
+                                </div>
+                                <p>
+                                    <b>
+                                        Resellers One Stop Shop for SMM Services
+                                        <br />
+                                    </b>
+                                    Full API Support, From Payment to Delivery
+                                    100% Hands Free!
+                                    <br />
+                                    <br />
+                                    <br />
+                                    Get <a href="#"> SMM Panel Script</a> and
+                                    start your own SMM Reseller business
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="growBox">
+                                <div class="growIcon">
+                                    <i class="fa fa-bolt"></i>
+                                </div>
+                                <p>
+                                    <b>Unique YouTube Views</b>
+                                    <br /> We Provide 100% Real &amp; Human
+                                    Viewers, NO Bots / Spam methods used, Our
+                                    YouTube Views and Live Stream Views help in
+                                    ranking and can be running with{" "}
+                                    <b>ADS ON</b>,<br /> We will never risk you
+                                    or your clients.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="growBox">
+                                <div class="growIcon">
+                                    <i class="fa fa-thumbs-up"></i>
+                                </div>
+                                <p>
+                                    <b>We Deliver Quality</b>
+                                    <br /> ★ Services Analyzed and Tested every
+                                    6 hours to make sure that your orders will
+                                    be delivered without issues. Best Quality by
+                                    the leaders in market
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="growBox">
+                                <div class="growIcon">
+                                    <i class="fa fa-tachometer"></i>
+                                </div>
+                                <p>
+                                    <b>Automate &amp; Schedule EVERYTHING!</b>
+                                    <br /> You can use smm panel to automate
+                                    your clients future needs without wasting
+                                    your time, also, using our drip feed option,
+                                    will make sure to deliver the service in the
+                                    natural way, Check out our options
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="growing-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h2>
+                                SnakersPanel smm panel <br /> Snakerspanel World
+                                Cheapest smm panel| smm reseller panel
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="growBox">
+                                <div class="growIcon">
+                                    <i class="fa fa-rocket"></i>
+                                </div>
+                                <p>
+                                    <b>
+                                        Resellers One Stop Shop for SMM Services
+                                        <br />
+                                    </b>
+                                    Full API Support, From Payment to Delivery
+                                    100% Hands Free!
+                                    <br />
+                                    <br />
+                                    <br />
+                                    Get <a href="#"> SMM Panel Script</a> and
+                                    start your own SMM Reseller business
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="growBox">
+                                <div class="growIcon">
+                                    <i class="fa fa-bolt"></i>
+                                </div>
+                                <p>
+                                    <b>Unique YouTube Views</b>
+                                    <br /> We Provide 100% Real &amp; Human
+                                    Viewers, NO Bots / Spam methods used, Our
+                                    YouTube Views and Live Stream Views help in
+                                    ranking and can be running with{" "}
+                                    <b>ADS ON</b>,<br /> We will never risk you
+                                    or your clients.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="growBox">
+                                <div class="growIcon">
+                                    <i class="fa fa-thumbs-up"></i>
+                                </div>
+                                <p>
+                                    <b>We Deliver Quality</b>
+                                    <br /> ★ Services Analyzed and Tested every
+                                    6 hours to make sure that your orders will
+                                    be delivered without issues. Best Quality by
+                                    the leaders in market
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="growBox">
+                                <div class="growIcon">
+                                    <i class="fa fa-tachometer"></i>
+                                </div>
+                                <p>
+                                    <b>Automate &amp; Schedule EVERYTHING!</b>
+                                    <br /> You can use smm panel to automate
+                                    your clients future needs without wasting
+                                    your time, also, using our drip feed option,
+                                    will make sure to deliver the service in the
+                                    natural way, Check out our options
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default GrowingSection;
