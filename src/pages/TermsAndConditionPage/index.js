afe10c1471b180b8a75dd0fa19c2b2fce5ea7f3d
krishnajhapate <ki54.jhapate@gmail.com>
2021-10-30 12:30:02 +0530

    added terms and condtion page

--- src/pages/TermsAndConditionPage/index.js
+++ src/pages/TermsAndConditionPage/index.js
@@ -4,7 +4,94 @@ import HeaderLanding from "../../components/HeaderLanding";
 function TermsAndConditionPage() {
     return (
         <div>
-            <HeaderLanding />n
+            <HeaderLanding />
+
+            <div className="container" style={{ marginTop: "10px" }}>
+                <section>
+                    <h2>Terms of Service snakerspanel</h2>
+                    <div>
+                        <p className="text-white">
+                            ✔ The use of services provided by snakerspanel smm
+                            panelestablishes agreement to these terms. By
+                            registering or using our services you agree that you
+                            have read and fully understood the following terms
+                            of Service and snakerspanel smm panelwill not be
+                            help liable for loss in any way for users who have
+                            not read the below terms of service.
+                        </p>
+                        <p className="text-white">
+                            ✔ snakerspanel does not guarantee a delivery time
+                            for any services. We offer our best estimation for
+                            when the order will be delivered. This is only an
+                            estimation and snakerspanel will not refund orders
+                            that are processing if you feel they are taking too
+                            long.
+                        </p>
+                        <p className="text-white">
+                            ✔ We reserve the right to change these terms of
+                            service without notice. You are expected to read all
+                            terms of service before placing every order to
+                            insure you are up to date with any changes or any
+                            future changes.
+                        </p>
+                        <p className="text-white">
+                            ✔ snkerspanel rates are subject to change at any
+                            time without notice. The payment/refund policy stays
+                            in effect in the case of rate changes.
+                        </p>
+                        <p className="text-white">
+                            ✔ Disclaimers : snakerspanel will not be responsible
+                            for any damages you or your business may suffer.
+                        </p>
+
+                        <p className="text-white">
+                            ✔ Liabilities : snakerspanel is in no way liable for
+                            any account suspension or picture deletion done by
+                            Instagram or Twitter or Facebook or YouTube or Other
+                            Social Media
+                        </p>
+                    </div>
+
+                    <div>
+                        <h2>Refund Policy</h2>
+                        <li className="text-white">
+                            No refunds will be made . After a deposit has been
+                            completed, there is no way to reverse it. You must
+                            use your balance on orders from snakerspanel
+                        </li>
+                        <li className="text-white">
+                            You agree that upon purchasing our services, that
+                            you clearly understand and agree what you are
+                            purchasing and will not file a fraudulent dispute
+                            via PayPal or any other Payment sources
+                        </li>
+                        <li className="text-white">
+                            Upon a fraudulent attempt to file a dispute, we
+                            receive the right, if necessary, to reset all
+                            followers and likes, terminate your account and/or
+                            permanently ban your IP address.
+                        </li>
+                        <li className="text-white">
+                            Fraudulent activity such as using unauthorized or
+                            stolen credit cards will lead to termination of your
+                            account. There are no exceptions.
+                        </li>
+                        <li className="text-white">
+                            Please do not use more than one server at the same
+                            time for the same page. We cannot give you correct
+                            followers/likes number in that case. We will not
+                            refund for these orders. Please keep attention!
+                        </li>
+                        <li className="text-white">
+                            we will refund for your canceled order in
+                            snakerspanel account only
+                        </li>
+                        <li className="text-white">
+                            We have Full Right To Change T&C Anytime
+                        </li>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
